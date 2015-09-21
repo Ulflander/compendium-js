85c4ceb33ecff669f3f2b2be50eb6350ad957e0f
louis <louis.chavane@gmail.com>
2015-09-21 16:56:08 +0200

    add french tagger specifics

---
+++ src/fr/tagger.specifics.js
@@ -0,0 +1,13 @@
+(function() {
+
+    var isPlural = compendium.inflector.isPlural;
+
+    extend(pos.specifics, {
+        DEFAULT_TAG: 'NOM',
+        EMOT_TAG: 'EM',
+        PUNC_TAG: '.',
+        SYM_TAG: 'SYM',
+        NUM_TAG: 'CD'
+      })
+
+}());
