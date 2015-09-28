6d52432ef94d4e48997d51664e5b2d7efbaa1e49
Ulflander <xlaumonier@gmail.com>
2015-09-28 12:48:32 +0200

    Run lexicon builder test only if build folder available (fix Travis-CI error)

--- test/fr/builder-lexicon-fr.js
+++ test/fr/builder-lexicon-fr.js
@@ -1,14 +1,29 @@
 
 // The following tests validates that the lexicon builder works.
 
+var fs = require('fs'),
+    exists = true;
 
-[{mode:'Build', path: '../../src/build/lexicon-fr.js'}].forEach(function(pkg) {
-    var Builder = require(pkg.path);
+// Run this test only in dev mode
+// because it won't work on Travis-ci
+
+try {
+    fs.statSync('../../build/');
+} catch(e) {
+    exists = false;
+}
+
+if (!!exists) {
+
+    [{mode:'Build', path: '../../src/build/lexicon-fr.js'}].forEach(function(pkg) {
+        var Builder = require(pkg.path);
 
 
-    exports[pkg.mode + ' mode  - Lexicon builder checks'] = function(test){
-        test.equal(!!Builder.getFullCompiled(), true, 'Full lexicon canno\'t be empty');
-        test.equal(!!Builder.getMinimalCompiled(), true, 'Full lexicon canno\'t be empty');
-        test.done();
-    };
-});
+        exports[pkg.mode + ' mode  - Lexicon builder checks'] = function(test){
+            test.equal(!!Builder.getFullCompiled(), true, 'Full lexicon canno\'t be empty');
+            test.equal(!!Builder.getMinimalCompiled(), true, 'Full lexicon canno\'t be empty');
+            test.done();
+        };
+    });
+
+}
